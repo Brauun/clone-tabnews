d77b032fa704b6e0fdebcdd7db82755b50302d33
Braian <70529300+Brauun@users.noreply.github.com>
2024-12-03 22:16:37 +0000

    adiciona o arquivo .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1 >Oi meu amor, como você está? <br/>
         Passando aqui para te agradecer e te dizer que sou muito admirado por você!<br/>
-        Te Amo muito! ❤
+        Te Amo muito! ❤❤
     </h1>
     
 }
